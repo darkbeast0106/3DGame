30db8ec86cb6f627bb07665a65de5273cff443f9
Budaházi Bence <budahazi.bence@petrik.hu>
2021-01-22 10:31:51 +0100

    Alap mozgás és nézelődés

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    public float MouseSensitivy = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Kurzor középre lesz lockolva és nem látszódik a játékban
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * MouseSensitivy;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * MouseSensitivy;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
     }
 }
